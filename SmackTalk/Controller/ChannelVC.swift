c2b4e49675e73a925d196b36cc2ea26698d84a02
Evan <Evan@evans-mbp-2.gateway.pace.com>
2017-07-23 09:18:49 -0400

    RevealViewController working

---
+++ SmackTalk/Controller/ChannelVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChannelVC.swift
+//  SmackTalk
+//
+//  Created by Evan on 7/23/17.
+//  Copyright © 2017 Evan Laird. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+
+
+}
---
+++ SmackTalk/Controller/ChatVC.swift
@@ -0,0 +1,27 @@
+//
+//  ChatVC.swift
+//  SmackTalk
+//
+//  Created by Evan on 7/23/17.
+//  Copyright © 2017 Evan Laird. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    
+    @IBOutlet weak var menuButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+
+    }
+
+}
