2cae46da9eeb1ad05efb30f378d5e0453b7a439b
Evan <Evan@evans-mbp-2.gateway.pace.com>
2017-07-23 10:43:18 -0400

    LoginVC Finished

---
+++ SmackTalk/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  SmackTalk
+//
+//  Created by Evan on 7/23/17.
+//  Copyright © 2017 Evan Laird. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+}
---
+++ SmackTalk/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  SmackTalk
+//
+//  Created by Evan on 7/23/17.
+//  Copyright © 2017 Evan Laird. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "ToLoginVC"
